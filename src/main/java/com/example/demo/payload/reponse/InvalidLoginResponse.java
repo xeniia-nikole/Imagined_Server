f503b74b8f6914c2a1af14493c79810379aa3584
Perevodova Kseniia <oliricat@gmail.com>
2022-10-20 17:47:54 +0300

    add security (authorisation)

---
+++ src/main/java/com/example/demo/payload/reponse/InvalidLoginResponse.java
@@ -0,0 +1,17 @@
+package com.example.demo.payload.reponse;
+
+import lombok.Getter;
+
+@Getter
+public class InvalidLoginResponse {
+
+    private final String username;
+    private final String password;
+
+    public InvalidLoginResponse() {
+        this.username = "Invalid Username";
+        this.password = "Invalid Password";
+    }
+
+
+}
