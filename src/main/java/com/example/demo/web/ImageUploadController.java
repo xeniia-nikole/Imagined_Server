736931ab0fb44f4545eb2af81c475dbef48f5a32
Perevodova Kseniia <oliricat@gmail.com>
2022-11-04 12:05:27 +0300

    add controllers

---
+++ src/main/java/com/example/demo/web/ImageUploadController.java
@@ -0,0 +1,51 @@
+package com.example.demo.web;
+
+import com.example.demo.model.Image;
+import com.example.demo.payload.reponse.MessageResponse;
+import com.example.demo.services.ImageUploadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.Principal;
+
+@RestController
+@RequestMapping("api/image")
+@CrossOrigin
+public class ImageUploadController {
+
+    @Autowired
+    private ImageUploadService imageUploadService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<MessageResponse> uploadImageToUser(@RequestParam("file") MultipartFile file,
+                                                             Principal principal) throws IOException {
+
+        imageUploadService.uploadImageToUser(file, principal);
+        return ResponseEntity.ok(new MessageResponse("Image Uploaded Successfully"));
+    }
+
+    @PostMapping("/{postId}/upload")
+    public ResponseEntity<MessageResponse> uploadImageToPost(@PathVariable("postId") String postId,
+                                                             @RequestParam("file") MultipartFile file,
+                                                             Principal principal) throws IOException {
+        imageUploadService.uploadImageToPost(file, principal, Long.parseLong(postId));
+        return ResponseEntity.ok(new MessageResponse("Image Uploaded Successfully"));
+    }
+
+    @GetMapping("/profileImage")
+    public ResponseEntity<Image> getImageForUser(Principal principal) {
+        Image userImage = imageUploadService.getImageToUser(principal);
+        return new ResponseEntity<>(userImage, HttpStatus.OK);
+    }
+
+    @GetMapping("/{postId}/image")
+    public ResponseEntity<Image> getImageToPost(@PathVariable("postId") String postId) {
+        Image postImage = imageUploadService.getImageToPost(Long.parseLong(postId));
+        return new ResponseEntity<>(postImage, HttpStatus.OK);
+    }
+
+}
